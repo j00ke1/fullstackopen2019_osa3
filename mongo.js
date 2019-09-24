1062193c85e0701395d10813661ff50a149eff78
Joona Kemppainen <joona.kemppainen@rokki.net>
2019-09-24 21:56:31 +0300

    3.12 valmis

---
+++ mongo.js
@@ -0,0 +1,41 @@
+const mongoose = require('mongoose');
+
+if (process.argv.length < 3) {
+  console.log('give password as argument');
+  process.exit(1);
+}
+
+const password = process.argv[2];
+const name = process.argv[3];
+const number = process.argv[4];
+
+const url = `mongodb+srv://admin:${password}@cluster0-uscpq.mongodb.net/test?retryWrites=true&w=majority`;
+
+mongoose.connect(url, { useNewUrlParser: true });
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: Number
+});
+
+const Person = mongoose.model('Person', personSchema);
+
+if (process.argv.length === 3) {
+  Person.find({}).then(result => {
+    console.log('Phonebook:');
+    result.forEach(person => {
+      console.log(`${person.name} ${person.number}`);
+    });
+    mongoose.connection.close();
+  });
+} else {
+  const newPerson = new Person({
+    name,
+    number
+  });
+
+  newPerson.save().then(response => {
+    console.log(`Added ${name} with number ${number} to the phonebook`);
+    mongoose.connection.close();
+  });
+}
